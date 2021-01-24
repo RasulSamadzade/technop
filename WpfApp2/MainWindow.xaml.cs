fbe4c934d062edb137ad8df1a3d6426f914d8ba2
Rasul Samadzade <rasul.samadzade@cognex.com>
2021-01-24 22:19:31 +0100

    logo added

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -34,6 +34,7 @@ namespace WpfApp2
             string connectionString = "Data Source="+ Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().Length - 9) + "TechnoProbe.db;Version=3;New=False;Compress=True;";
             sqlConnection =new SQLiteConnection(connectionString);
             PlateTypes.ItemsSource = new List<String>() { "U1", "U2", "M1", "M2", "L1", "L2" };
+            Logo.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().Length - 9) + "Image\\Logo.jpeg", UriKind.Absolute));
             showTypes();
         }
 
